0a08cd4fd458d082a7cbf4a55d9b858a83f0a85f
Willard Kluit <99040651@mydavinci.nl>
2017-01-17 13:39:48 +0100

    Added comments

---
+++ adventure-html.js
@@ -0,0 +1,34 @@
+confirm("I'm ready!");
+
+var age = prompt("What's your age");
+
+if  (age < 13) {
+    console.log ("Play at your own risk!");
+}
+else { 
+    console.log ("Have fun playing!");
+}
+
+console.log ("You are at a Justin / concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+
+console.log ("Suddenly, / stops and says, 'Who wants to race me?'");
+
+var userAnswer = prompt("Do you want to race / on stage?");
+
+if (userAnswer === "yes") {
+    "You and / start racing. It's neck and neck! You win by a shoelace!"
+}
+else {
+    "Oh no! / shakes his head and sings 'I set a pace, so I can race without pacing.'"
+}
+
+var feedback = prompt("Please rate our game on a scale from 1 to 10")
+
+if (feedback > 8) {
+    "Thank you! We should race at the next concert!"
+}
+else {
+    "I'll keep practicing coding and racing."
+}
+
+//Placeholder text, copied
